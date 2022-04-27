34edb9d5e6c8eb76d546a4b42e18b37223a3a7c6
Thomas <TFox95@aol.com>
2022-04-26 22:17:38 -0500

    added Account to authLinks and  Home to AnonLinks within Navbar. changed navbar.Brand name

--- gui/src/hocs/Navbar.jsx
+++ gui/src/hocs/Navbar.jsx
@@ -5,6 +5,11 @@ const Navbar = () => {
 
     const anonLinks = (
         <Fragment>
+            <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link" exact to="/" >Home</NavLink>
+                </li>
+            </ul>
             <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
                     <NavLink className="nav-link" to="/login">Login</NavLink>
@@ -17,16 +22,37 @@ const Navbar = () => {
         </Fragment>
     );
 
+    const authLinks = (
+        <Fragment>
+            <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link" exact to="/" >Dashboard</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/create" >Create Todo</NavLink>
+                </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link" exact to="/Account" >Account</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" exact to="#!" >Logout</NavLink>
+                </li>
+            </ul>
+        </Fragment>
+    )
+
     return (
         <div className="container-fullwidth">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" exact to="/">Fox Notes</NavLink>
+                    <NavLink className="navbar-brand" exact to="#">Asterisks</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                            {anonLinks}
+                        {anonLinks}
                     </div>
                 </div>
             </nav>
