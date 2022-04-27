0d34f3c26aa62529a0eb914984d5f6dd7ae5c5b8
Thomas <TFox95@aol.com>
2022-04-26 20:46:49 -0500

    created descriptive Card on homepage. alsoadded bootstrap js script in index.html

--- gui/src/containers/Home.jsx
+++ gui/src/containers/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
 
     return (
-        <div>
-            <p>Home</p>
+        <div className="container">
+            <section className="mt-5 p-5 bg-light">
+                <h1>Welcome to *Asterisks*</h1>
+                <p className="lead">
+                    This is a S.P.A; with Django(Python) REST as the backend and React(JSX) as the frontend
+                </p>
+                <hr className="my-4" />
+                <p>Click the button below to log in</p>
+                <Link className="btn btn-primary btn-lrg" to="/login">Login</Link>
+            </section>
         </div>
     );
 };
