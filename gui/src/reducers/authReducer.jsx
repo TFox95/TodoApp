3979be28ffbc269c2164aba46a298e951e9d7783
Thomas <TFox95@aol.com>
2022-05-14 20:04:16 -0500

    named unnamed function in authReducer.jsx to auth

--- gui/src/reducers/authReducer.jsx
+++ gui/src/reducers/authReducer.jsx
@@ -11,7 +11,7 @@ const initialState = {
     token: null
 };
 
-export default function (state = initialState, action) {
+export default function auth (state = initialState, action) {
     const { type, payload } = action;
 
     switch (type) {
