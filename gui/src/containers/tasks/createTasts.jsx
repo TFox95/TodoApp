28a0fbd6d7ad177762aa412cee1fe450948a0a3e
Thomas <TFox95@aol.com>
2022-05-30 18:32:36 -0500

    seperated span's for added readability

--- gui/src/containers/tasks/createTasts.jsx
+++ gui/src/containers/tasks/createTasts.jsx
@@ -10,14 +10,17 @@ const CreateTask = ({ isAuthenticated, token }) => {
                 <p className="lead"> Create your React-Powered Task</p>
 
                 <form className="form-group">
+
                     <span>
                         <label for="Task-Title">Task's Title</label>
                         <input title="Example: Change Oil" type="text" className="form-control" id="Task-Title" aria-describedby="titleHelp" placeholder="Enter Task's Title"/>
                     </span>
+
                     <span>
                         <label for="TasksDesc">Description</label>
                         <textarea type="text" className="form-control" id="TasksDesc" placeholder="place description here..." />
                     </span>
+
                     <span >
                         <label for="form-category">Category</label>
                         <select className="form-select" aria-label=".form-select-lg example" id="form-category">
@@ -27,6 +30,7 @@ const CreateTask = ({ isAuthenticated, token }) => {
                             <option value="3">Three</option>
                         </select>
                     </span>
+
                     <span>
                         <label for="form-priority" >Priority</label>
                         <select className="form-select" aria-label=".form-select-lg example" id="form-category">
@@ -36,10 +40,12 @@ const CreateTask = ({ isAuthenticated, token }) => {
                             <option value="3">Three</option>
                         </select>
                     </span>
+
                     <span className id="datepicker">
                         <label for="date" className="col-sm-1 col-form-label">Date</label>
                         <input type="date" className="form-control" id="date" />
                     </span>
+                    
                     <span className="mb-3 mt-3 form-check">
                         <label className="form-check-label" for="exampleCheck1">Check me out</label>
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
