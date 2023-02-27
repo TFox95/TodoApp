7c481cd9b179d21c26eb0368c2847497b77b9dff
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2023-02-27 07:42:56 -0600

    created new variable named prevScrollpos to get the windows page Y position and created a new function from the window.onscroll function to handle the dynamic navbar.

--- Gui-Redesign/app.js
+++ Gui-Redesign/app.js
@@ -3,12 +3,28 @@ const smartMenu = document.querySelector("#smart-menu");
 const smartMenuLinks = document.querySelector(".nav-flex-end");
 const LearnNowScroll = document.querySelector(".hero-btn button");
 const scrollHere = document.querySelector("body")
+let prevScrollpos = window.pageYOffset;
+
 
 // functions below
 smartMenu.addEventListener("click", () => {
     smartMenu.classList.toggle("is-active")
     smartMenuLinks.classList.toggle("active")
 })
+
+scrollHere.addEventListener("scroll", () => {
+    scrollHere.style.setProperty("overflow", "visible")
+})
+
+window.onscroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navbar").style.setProperty("top", "0")
+    } else {
+        document.querySelector(".navbar").style.setProperty("top", "-4rem")
+    }
+    prevScrollpos = currentScrollPos
+}
 
 if (LearnNowScroll) {
     LearnNowScroll.addEventListener("click", () => {
@@ -17,8 +33,3 @@ if (LearnNowScroll) {
         })
     })
 }
-
-
-scrollHere.addEventListener("scroll", () => {
-    scrollHere.style.setProperty("overflow", "visible")
-})
