ac42c8aae562bcbf5bc7512fb4aa014a3c4916e9
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2023-07-17 12:52:36 -0500

    v0.1
    Added type definitions to props and also passed the props.route to the Navlink to attribute.

--- gui-functional-redesign/src/Components/dashComp/taskCard.tsx
+++ gui-functional-redesign/src/Components/dashComp/taskCard.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { NavLink } from "react-router-dom";
+import { TaskCardInterface } from "../../lib/interfaces/dash/Interfaces";
 
-const TaskCard = (props: any): React.ReactNode => {
+const TaskCard = (props: TaskCardInterface): React.ReactNode => {
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <p>{props.description}</p>
-      <NavLink to="#">View Details</NavLink>
+      <NavLink to={props.route}>View Details</NavLink>
     </div>
   );
 };
