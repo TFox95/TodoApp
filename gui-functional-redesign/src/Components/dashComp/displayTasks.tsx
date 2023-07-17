a11ece3a97b18d04229a7f09b188aa7d55f8953b
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2023-07-17 12:58:52 -0500

    v0.1
    Added more Boilerplate to the TaskArray function, with adding all necessary attrs.

--- gui-functional-redesign/src/Components/dashComp/displayTasks.tsx
+++ gui-functional-redesign/src/Components/dashComp/displayTasks.tsx
@@ -2,24 +2,30 @@ import * as React from "react";
 import TaskCard from "./taskCard";
 
 
+
 const taskArray = (taskArray = Array()): React.ReactNode => {
+    
+    const [tasks, setTasks] = React.useState(taskArray)
+    console.log(tasks, setTasks)
 
     if (taskArray.length)
         return taskArray.map((index) => {
             return (
                 <TaskCard
+                    key={index.title}
                     title={index.title}
                     description={index.desc}
-                    key={index.title}
+                    route="#"
                 />
             );
         }
         );
     if (!taskArray.length)
         return <TaskCard
+            key={"Demo Task"}
             title={"Demo Task"}
             description={"This is a demo task to give you an example of what you would be seeing"}
-            key={"Demo Task"}
+            route={"#"}
         />;
 };
 
