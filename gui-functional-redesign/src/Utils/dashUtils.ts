a0e91d9c18a0ee11125a4bf806c148f8545dcbd3
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2023-07-17 12:39:45 -0500

    v0.1
    Added type annotations to the various functions within this module. Also created a new function which will be more defined when I start writing State logic, named handleFilter.

--- gui-functional-redesign/src/Utils/dashUtils.ts
+++ gui-functional-redesign/src/Utils/dashUtils.ts
@@ -1,61 +1,84 @@
-
-const updateDate = () => {
+const updateDate: () => void = () => {
   const dateElem = document.getElementById("date")!;
   const currentDate = new Date();
   dateElem.innerHTML = currentDate.toDateString()!;
   setTimeout(updateDate, 30000);
 };
 
-const sidebarToggle = () => {
+const sidebarToggle: () => void = () => {
   const sideNav = document.getElementById("mySidenav")! as HTMLElement;
 
   switch (sideNav.style.width) {
-
-    case ("230px"):
+    case "230px":
       sideNav.style.width = "65px";
       break;
 
-    case ("65px"):
+    case "65px":
     default:
       sideNav.style.width = "230px";
       break;
-  };
+  }
 };
 
-
-const sortToggle = () => {
+const sortToggle: () => void = () => {
   const altNav = document.querySelector(".sort-container")! as HTMLElement;
-  const altNavdisplayed = document.querySelector(".sort-section")! as HTMLElement;
+  const altNavdisplayed = document.querySelector(
+    ".sort-section"
+  )! as HTMLElement;
 
   switch (altNav.style.width) {
-
-    case ("230px"):
+    case "230px":
       altNavdisplayed.style.opacity = "0";
       console.log("okay");
       altNav.style.width = "0px";
       break;
 
-    case ("0px"):
+    case "0px":
     default:
       altNavdisplayed.style.opacity = "1";
       console.log("huh?");
       altNav.style.width = "230px";
       break;
-  };
+  }
 };
 
-const hideFooter = () => {
+const hideFooter: () => void = () => {
   window.addEventListener("load", () => {
     const footer = document.querySelector(".footer") as HTMLElement;
     if (footer) {
       footer.style.display = "none";
     }
-  })
+  });
 };
 
-export {
-  updateDate,
-  sidebarToggle,
-  hideFooter,
-  sortToggle,
-}
+const handleFilter = (option: string, tasks: any[]) => {
+  // Perform sorting logic based on the option
+  // Update the tasks state with the sorted array
+  // You can customize the sorting logic as per your requirements
+  let filteredTasks: any[] = [];
+  console.log(tasks);
+
+  switch (option) {
+    default:
+    case "myday":
+      filteredTasks = tasks;
+      break;
+    case "important":
+      filteredTasks = [...tasks].sort((a, b) =>
+        a.description.localeCompare(b.description)
+      );
+      break;
+    case "completed":
+      filteredTasks = [...tasks].sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    case "overdue":
+      filteredTasks = [...tasks].sort((a, b) =>
+        a.description.localeCompare(b.description)
+      );
+      break;
+  }
+
+  return filteredTasks;
+};
+
+export { updateDate, sidebarToggle, hideFooter, sortToggle, handleFilter };
