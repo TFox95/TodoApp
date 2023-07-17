d09defeea37e023b8704e1023fc6b7f420ab302b
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2023-07-17 12:44:27 -0500

    v0.05 bugfix
    Added, formatting.

--- gui-functional-redesign/src/Pages/Dashboard.tsx
+++ gui-functional-redesign/src/Pages/Dashboard.tsx
@@ -6,28 +6,38 @@ import { hideFooter } from "../Utils/dashUtils";
 import taskArray from "../Components/dashComp/displayTasks";
 
 const Dashboard = (): React.ReactNode => {
-    hideFooter();
-    const [tasks, setTasks] = React.useState(new Array({ "title": "Title 1", "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { "title": "Task 2", "desc": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-    { "title": "Task 3", "desc": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
-    { "title": "Task 4", "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit nostrum incidunt perferendis eaque unde necessitatibus. Omnis atque facilis, exercitationem sequi hic, in non voluptatibus dolor sapiente quod nostrum ex debitis." }))
+  hideFooter();
+  const tasks = new Array(
+    {
+      title: "Title 1",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      title: "Task 2",
+      desc: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "Task 3",
+      desc: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      title: "Task 4",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit nostrum incidunt perferendis eaque unde necessitatibus. Omnis atque facilis, exercitationem sequi hic, in non voluptatibus dolor sapiente quod nostrum ex debitis.",
+    }
+  );
 
-    return (
-        <div className="dashboard">
-            <div className="dashboard-container">
-                <Sidebar
-                    position={"Left"} />
-                <div className="dashboard-content">
-                    <SortDateOptions />
-                    <div className="dashboard-content-block">
-                        {taskArray(tasks)}
-                    </div>
-                </div>
-                <Sidebar
-                    position={"Right"} />
-            </div>
+  return (
+    <div className="dashboard">
+      <div className="dashboard-container">
+        <Sidebar position={"Left"} />
+        <div className="dashboard-content">
+          <SortDateOptions />
+          <div className="dashboard-content-block">{taskArray(tasks)}</div>
         </div>
-    );
+        <Sidebar position={"Right"} />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
